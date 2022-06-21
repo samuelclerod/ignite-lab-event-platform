75ea551e00c8fd9a2933c679e62a28cd72e0eec2
Samuel Rodrigues <samuelclerod@gmail.com>
2022-06-21 08:53:03 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import { gql, useQuery } from "@apollo/client";
+
+
+const GET_lESSONS_QUERY = gql`
+  query {
+    lessons {
+      id
+      title
+    }
+  }
+`;
+
+interface Lesson {
+  id: string;
+  title: string;
+}
+
+function App() {
+
+  const { data } = useQuery<{ lessons: Lesson[] }>(GET_lESSONS_QUERY);
+
+
+  return (
+    <ul>
+      {
+        data?.lessons.map(lesson => {
+          return <li key={lesson.id}>{lesson.title}</li>
+        })
+      }
+    </ul>
+  )
+}
+
+export default App
